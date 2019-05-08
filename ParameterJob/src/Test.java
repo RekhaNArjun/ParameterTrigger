4da0da6e701a2cdb2dfe0a2a1617c1b49decaaf0
rekha <rekhaar@cybage.com>
2019-05-08 18:59:58 +0530

    param_trigger project added

---
+++ ParameterJob/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main()
+	{
+		int d=getValue();
+		System.out.println(d);
+	}
+public static int getValue()
+{
+	return 3+2 ;
+}
+}
---
+++ ParameterJob/src/Trigger.java
@@ -0,0 +1,9 @@
+
+public class Trigger {
+	public static void main()
+	{
+		int dd=Test.getValue();
+	}
+	
+
+}
